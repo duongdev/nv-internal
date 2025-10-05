e38986af7bf1cfa6b77adbbfb74cc2dfbd126e7c
Dustin Do <dustin.do95@gmail.com>
2025-10-05 18:03:11 +0700

    feat(api): Setup vercel deploy

--- apps/api/src/lib/prisma.ts
+++ apps/api/src/lib/prisma.ts
@@ -1,20 +1,27 @@
-/** biome-ignore-all lint/style/useNamingConvention: <extend model name> */
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { neonConfig } from '@neondatabase/serverless'
+import { PrismaNeon } from '@prisma/adapter-neon'
 import { extendPrismaClient } from 'prisma-prefixed-ids'
+import ws from 'ws'
 import { type Prisma, PrismaClient } from '../../generated/prisma'
+
+neonConfig.webSocketConstructor = ws
+neonConfig.poolQueryViaFetch = true
 
 type ModelName = Prisma.ModelName
 
 const prefixes: Partial<Record<ModelName, string>> = {
+  /** biome-ignore-start lint/style/useNamingConvention: <extend model name> */
   Customer: 'cust',
   GeoLocation: 'geo',
   Activity: 'act',
+  /** biome-ignore-end lint/style/useNamingConvention: <extend model name> */
 }
 
 export const getPrisma = (databaseUrl = process.env.DATABASE_URL) => {
+  const adapter = new PrismaNeon({ connectionString: databaseUrl })
   const prisma = new PrismaClient({
-    datasourceUrl: databaseUrl,
-  }).$extends(withAccelerate())
+    adapter,
+  })
   return extendPrismaClient(prisma, {
     prefixes,
   }) as unknown as PrismaClient
